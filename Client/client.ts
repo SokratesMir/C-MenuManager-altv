4d12ae3b3af4bfa645d6f16959b9c0fd50b5e3d9
Sokrates <32365778+SokratesMir@users.noreply.github.com>
2019-08-23 12:41:19 +0200

    Initial commit

---
+++ Client/client.ts
@@ -0,0 +1,26 @@
+﻿/// <reference path="typings/altv-client.d.ts"/>
+/// <reference path="typings/natives.d.ts"/>
+
+import * as alt from 'alt';
+import * as game from 'natives';
+
+import menuManager from 'scripts/MenuManager';
+
+const init = async () => {
+    try {
+        let count = 0;
+		
+        await menuManager();
+        count++;
+
+        alt.log(`All ${count} scripts loaded successfully.`);
+    }
+    catch (Exception)
+    {
+        alt.logError(`Failed to load scripts.\nMessage: ${Exception.Message}`);
+        game.freezeEntityPosition(alt.Player.local.scriptID, true);
+        game.doScreenFadeOut(0);
+        alt.logError(`Blocking spawning! Try to reconnect, if the problem happens again please contact the serverowner.`);
+    }
+};
+init();
---
+++ Client/index.mjs
@@ -0,0 +1,1 @@
+﻿//Empty like my life
